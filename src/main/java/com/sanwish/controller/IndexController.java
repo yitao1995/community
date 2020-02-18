13da47620e7fff83f3a94f8d19c61e6f01b0bcb8
yitao1995 <331617048@qq.com>
2020-02-19 05:07:36 +0800

    add index and bootstrap

--- src/main/java/com/sanwish/controller/IndexController.java
+++ src/main/java/com/sanwish/controller/IndexController.java
@@ -9,14 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created by Sanwish on 2020/2/19.
  */
 @Controller
-public class HelloController {
+public class IndexController {
 
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name ,Model model){
-
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
 
     }
 }
